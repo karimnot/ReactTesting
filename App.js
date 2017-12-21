fc1bed0c06a6d678871e91485e757a9ba5032784
DANIEL KARIM RICARDEZ ORTIZ <ikarimnot@DANIELs-MacBook-Pro.local>
2017-12-21 15:56:17 -0600

    Style

--- App.js
+++ App.js
@@ -7,16 +7,18 @@ import Props from './components/Props';
 import PropsStyle from './components/PropsStyle';
 import LotsOfGreetings from './components/PropsMessages';
 import States from './components/States';
+import StylesApp from './components/StylesApp'
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        {/* <HelloWorld /> */}
-        {/* <Props /> */}
+        <HelloWorld /> 
+        <Props /> 
         <PropsStyle /> 
         <LotsOfGreetings />
         <States />
+        <StylesApp />
         
       </View>
     );
