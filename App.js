84e3a3348df68943b4c022b1faa07d2c852e6c76
DANIEL KARIM RICARDEZ ORTIZ <ikarimnot@DANIELs-MacBook-Pro.local>
2017-12-26 13:57:59 -0600

    Layout options

--- App.js
+++ App.js
@@ -7,15 +7,17 @@ import Props from './components/Props';
 import PropsStyle from './components/PropsStyle';
 import LotsOfGreetings from './components/PropsMessages';
 import States from './components/States';
-import StylesApp from './components/StylesApp'
-import Dimension from './components/Dimension'
-import FlexDimension from './components/FlexDimension'
+import StylesApp from './components/StylesApp';
+import Dimension from './components/Dimension';
+import FlexDimension from './components/FlexDimension';
+import LayoutFlexDirection from './components/LayoutFlexDirection';
+import LayoutJustifyContent from './components/LayoutJustifyContent';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        {/*}
+        {/*
         <HelloWorld /> 
         <Props /> 
         <PropsStyle /> 
@@ -23,9 +25,10 @@ export default class App extends Component {
         <States />
         <StylesApp />
         <Dimension />
-        */}
         <FlexDimension />
-
+        <LayoutFlexDirection />
+        */}
+        <LayoutJustifyContent />
       </View>
     );
   }
